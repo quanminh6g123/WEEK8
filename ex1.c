7aa95b72dabc6c17f706cac2ca81b50d92a9fa24
quanminh6g123 <134413545+quanminh6g123@users.noreply.github.com>
2023-05-24 00:09:11 +0700

    Update ex1.c
    new

--- ex1.c
+++ ex1.c
@@ -3,6 +3,7 @@ int main ()
 {
     int x=3;
     float number=3.4;
+    int a = 4;
     char ch ='a';
     printf("Value of x=%d, Address = %x\n",x,&x);
     printf("Value of number=%d, Address = %x\n",number,&number);
